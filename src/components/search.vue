17a7e3d646ab6413b758a86b10b1de3b11fd4eb5
tongbin <1020090463@qq.com>
2017-07-21 19:51:41 +0800

    完成搜索功能

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,7 +1,13 @@
 <template>
 	<div id="box">
-		<input ref="inp" autofocus @blur="getFocus" class="searchInp" type="text" v-model="keyWord" @keyup="get($event)" @keydown.down="down" @keydown.up="up"/>
+		<div class="search_pannel">
+			<!--搜索框，ref为真实的DOM节点，便于当失去焦点的时候，找回焦点-->
+			<input ref="inp" autofocus @blur="getFocus" class="searchInp" type="text" v-model="keyWord" @keyup="get($event)" @keydown.down="down" @keydown.up="up" @keydown.enter="search"/>
+			<!--清空输入框中内容，输入框中由内容才会显示-->
+			<span id="keyWordReset" @click="resetkeyWord" v-if="keyWord">&times;</span>
+		</div>
 		<div class="searchContent">
+			<!--过渡，使用ul包裹元素-->
 			<transition-group name="itemfade" tag="ul" mode="out-in" v-cloak>
 				<li v-for="(v,i) in myData" :key="v" :class="{active:activeIndex==i,hover:hoverIndex==i}" @mouseover="over(i)" @mouseout="out" @click="sel(v,i)">{{v}}</li>
 			</transition-group>
@@ -18,20 +24,28 @@
 				keyWord:"",
 				activeIndex:-1,
 				hoverIndex:-1,
+				searchTypeIndex:0,
+				searchType: [{
+	                name: '360搜索',
+	                searchSrc: 'https://www.so.com/s?ie=utf-8&shb=1&src=360sou_newhome&q='
+	            }, {
+	                name: '百度搜索',
+	                searchSrc: 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd='
+	            }, {
+	                name: '搜狗搜索',
+	                searchSrc: 'https://www.sogou.com/web?query='
+	            }]
 			}
 		},
 		methods:{
 			get:function(ev){
 				var that = this;
 				if( ev.keyCode == 38 || ev.keyCode == 40 ) return;
-//				console.dir( jsonp );
 				jsonp('https://sug.so.360.cn/suggest?word=' + this.keyWord + '&encodein=utf-8&encodeout=utf-8',{},function(res,data) {
 					that.myData = data.s;
-//					console.log( that.myData );
            		});
 			},
 			getFocus:function(){
-				console.log( 1 )
 				this.$refs.inp.focus();
 			},
 			over:function(a){
@@ -40,17 +54,31 @@
 			up:function(){
 				this.activeIndex--;
 				if(this.activeIndex<0)this.activeIndex = this.myData.length-1;
+				this.keyWord = this.myData[this.activeIndex];
 			},
 			down:function(){
 				this.activeIndex++;
 				this.activeIndex %= this.myData.length;
+				this.keyWord = this.myData[this.activeIndex];
 			},
 			out:function(){
 				this.hoverIndex = -1;
 			},
 			sel:function(a,b){
+				var that = this;
 				this.activeIndex = b;
 				this.keyWord = a;
+				setTimeout(that.search,500)
+			},
+			resetkeyWord:function(){
+				var that = this;
+				this.keyWord = "";
+				jsonp('https://sug.so.360.cn/suggest?word=' + this.keyWord + '&encodein=utf-8&encodeout=utf-8',{},function(res,data) {
+					that.myData = data.s;
+           		});
+			},
+			search:function(){
+				window.open( this.searchType[this.searchTypeIndex].searchSrc + this.keyWord )
 			}
 		}
 	}
@@ -73,7 +101,7 @@
 		width: 500px;
 		text-indent: 20px;
 		outline: none;
-		font: 20px/50 "微软雅黑";
+		font: 20px/50px "微软雅黑";
 	}
 	.searchContent{
 		width: 500px;
@@ -112,5 +140,20 @@
 	    height:0px
 	    
 	}
-
+	
+	#keyWordReset{
+		position: absolute;
+		right: 20px;
+		top: 6px;
+		font: bold 30px "微软雅黑";
+		cursor: pointer;
+	}
+	#keyWordReset:hover{
+		color: #ccc;
+	}	
+	.search_pannel {
+		border: 1px solid #FF4500;
+		display: inline-block;
+		position: relative;
+	}
 </style>
